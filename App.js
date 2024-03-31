77f2b18d240aa8326ebdb5816ecc39db50802f30
Hector Daniel Gonzalez Laport <79027049+DangoYT@users.noreply.github.com>
2024-03-31 13:33:10 -0300

    Experimentando

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo</Text>
+      <TouchableWithoutFeedback onPress={() => Alert.alert('Hola Mundo')}>
+        <Text>Hola Mundo</Text>
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </View>
   );
